f68c7c32d95ef86000c8a04034b69ef8d2c0908d
samis1988 <61112146+samis1988@users.noreply.github.com>
2026-03-04 10:08:53 +0300

    first commit

---
+++ ApiQap/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using ApiQap.Models;
+
+namespace ApiQap.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            List<User> users = new List<User>();
+
+            using (SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Users", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    users.Add(new User
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        Email = reader["Email"].ToString()
+                    });
+                }
+            }
+
+            return Ok(users);
+        }
+    }
+}
---
+++ ApiQap/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiQap.Models
+{
+    public class User
+    {
+        public int Id { get; set; }      // رقم المستخدم
+        public string Name { get; set; } // اسم المستخدم
+        public string Email { get; set; } // إيميل المستخدم
+    }
+}
